f1e6584d926bcb58d904aa9f6de61d26875ae42c
hondamarlboro <hondamarlboromclaren@gmail.com>
2014-06-01 23:39:05 +0900

    Update Feed2Reeder2.js

--- Feed2Reeder2.js
+++ Feed2Reeder2.js
@@ -2,7 +2,7 @@ javascript:(function(){
 	var d=document;
 	var el=d.getElementsByTagName('link');
 	for(i=0;i<el.length;i++){
-		if(el%5Bi%5D.getAttribute('rel').indexOf('alternate')!=-1){
+		if(el[i].getAttribute('rel').indexOf('alternate')!=-1){
 			ty=el[i].getAttribute('type');
 			if(ty&&(ty.indexOf('application/rss+xml')!=-1||ty.indexOf('text/xml')!=-1||ty.indexOf('application/atom+xml')!=-1)){
 				h=el[i].getAttribute('href');
